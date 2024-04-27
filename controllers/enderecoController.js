87d8c1c5bc0c577a914e4c499d5cc7c52ed2473a
victorhugoRUB <victor.santos@rubcube.com>
2024-04-27 15:37:03 -0300

    feat: CADASTRO E LISTAGEM DE ENDERECO

--- controllers/enderecoController.js
+++ controllers/enderecoController.js
@@ -1,6 +1,100 @@
+const { DateTime } = require("luxon");
 const EnderecoModel = require("../models/enderecoModel");
 
 class EnderecoController {
+
+    cadastroView(req, res) {
+        res.render('cadastrar/endereco')
+    }
+
+    async cadastrar(req, res) {
+        const dataHoje = DateTime.now();
+        console.log(req.body)
+        if (req.body.cep != "" && req.body.rua != "" && req.body.num != "" && req.body.bairro != "" && req.body.cidade != "" && req.body.estado != "" && req.body.complem != "" && req.body.pess_id != "") {
+            let endereco = new EnderecoModel(0, req.body.cep, req.body.rua, req.body.bairro, req.body.num, req.body.cidade, req.body.estado, req.body.complem, req.body.pess_id, dataHoje.toISODate(), dataHoje.toISODate());
+            let result = await endereco.cadastrarEndereco();
+
+            if (result) {
+                res.send({
+                    ok: true,
+                    msg: "Endereco cadastrado com sucesso!"
+                });
+            }
+            else {
+                res.send({
+                    ok: false,
+                    msg: "Erro ao cadastrar o endereco"
+                });
+            }
+        }
+        else {
+            res.send({
+                ok: false,
+                msg: "Parâmetros preenchidos incorretamente!"
+            });
+        }
+    }
+
+    async listagemView(req, res) {
+        let endereco = new EnderecoModel();
+        let listaEndereco = await endereco.listarEndereco()
+        res.render('listar/endereco', { listaEndereco: listaEndereco })
+    }
+
+    async alterarView(req, res) {
+        let endereco = new EnderecoModel();
+        endereco = await endereco.obterEndId(req.params.id);
+        res.render('alterar/endereco', { endereco: endereco });
+    }
+
+    async alterar(req, res) {
+        const dataHoje = DateTime.now()
+        const dataTratar = new Date(Date.parse(req.body.createdAt))
+        const dataTratar2 = DateTime.fromJSDate(dataTratar)
+        const dataCriacao = dataTratar2.toISODate()
+        console.log(req.body)
+        if (req.body.nome != "" && req.body.cpf != "" && req.body.rg != "" && req.body.nasc != "" && req.body.nacio != "" && req.body.genero != "" && req.body.tel != "" && req.body.tipo != "") {
+            let pessoa = new EnderecoModel(req.body.id, req.body.nome, req.body.cpf, req.body.rg, req.body.nasc, req.body.nacio, req.body.genero, req.body.tel, req.body.tipo, dataCriacao, dataHoje.toISODate());
+
+            let result = await pessoa.editarPessoa();
+
+            if (result) {
+                res.send({
+                    ok: true,
+                    msg: "Pessoa alterada com sucesso!"
+                });
+            }
+            else {
+                res.send({
+                    ok: false,
+                    msg: "Erro ao alterar pessoa!"
+                });
+            }
+        }
+        else {
+            res.send({
+                ok: false,
+                msg: "Parâmetros preenchidos incorretamente!"
+            });
+        }
+    }
+
+    async excluir(req, res) {
+        if (req.body.id != null) {
+            let pessoa = new EnderecoModel();
+            let ok = await pessoa.excluir(req.body.id);
+
+            if (ok) {
+                res.send({ ok: true });
+            }
+            else {
+                res.send({ ok: false, msg: "Erro ao excluir pessoa" })
+            }
+        }
+        else {
+            res.send({ ok: false, msg: "O id para exclusão não foi enviado" })
+        }
+    }
 
 }
 
--- models/enderecoModel.js
+++ models/enderecoModel.js
@@ -12,36 +12,39 @@ class EnderecoModel {
     #end_cidade;
     #end_estado;
     #end_complemento;
+    #pess_id;
     #createdAt;
     #updatedAt;
 
     // Getters
-    get end_id() { return this.end_id }
-    get end_cep() { return this.end_cep }
-    get end_rua() { return this.end_rua }
-    get end_bairro() { return this.end_bairro }
-    get end_numero() { return this.end_numero }
-    get end_cidade() { return this.end_cidade }
-    get end_estado() { return this.end_estado }
-    get end_complemento() { return this.end_complemento }
-    get createdAt() { return this.createdAt }
-    get updatedAt() { return this.updatedAt }
+    get end_id() { return this.#end_id }
+    get end_cep() { return this.#end_cep }
+    get end_rua() { return this.#end_rua }
+    get end_bairro() { return this.#end_bairro }
+    get end_numero() { return this.#end_numero }
+    get end_cidade() { return this.#end_cidade }
+    get end_estado() { return this.#end_estado }
+    get end_complemento() { return this.#end_complemento }
+    get pess_id() { return this.#pess_id }
+    get createdAt() { return this.#createdAt }
+    get updatedAt() { return this.#updatedAt }
 
     // Setters
-    set end_id(value) { this.end_id = value }
-    set end_cep(value) { this.end_cep = value }
-    set end_rua(value) { this.end_rua = value }
-    set end_bairro(value) { this.end_bairro = value }
-    set end_numero(value) { this.end_numero = value }
-    set end_cidade(value) { this.end_cidade = value }
-    set end_estado(value) { this.end_estado = value }
-    set end_complemento(value) { this.end_complemento = value }
-    set createdAt(value) { this.createdAt = value }
-    set updatedAt(value) { this.updatedAt = value }
+    set end_id(value) { this.#end_id = value }
+    set end_cep(value) { this.#end_cep = value }
+    set end_rua(value) { this.#end_rua = value }
+    set end_bairro(value) { this.#end_bairro = value }
+    set end_numero(value) { this.#end_numero = value }
+    set end_cidade(value) { this.#end_cidade = value }
+    set end_estado(value) { this.#end_estado = value }
+    set end_complemento(value) { this.#end_complemento = value }
+    set pess_id(value) { this.#pess_id = value }
+    set createdAt(value) { this.#createdAt = value }
+    set updatedAt(value) { this.#updatedAt = value }
 
     // Constructor
 
-    constructor(end_id, end_cep, end_rua, end_bairro, end_numero, end_cidade, end_estado, end_complemento, createdAt, updatedAt) {
+    constructor(end_id, end_cep, end_rua, end_bairro, end_numero, end_cidade, end_estado, end_complemento, pess_id, createdAt, updatedAt) {
 
         this.#end_id = end_id;
         this.#end_cep = end_cep;
@@ -51,6 +54,7 @@ class EnderecoModel {
         this.#end_cidade = end_cidade;
         this.#end_estado = end_estado;
         this.#end_complemento = end_complemento;
+        this.#pess_id = pess_id;
         this.#createdAt = createdAt;
         this.#updatedAt = updatedAt;
 
@@ -66,7 +70,7 @@ class EnderecoModel {
         let lista = [];
 
         for (let i = 0; i < rows.length; i++) {
-            lista.push(new EnderecoModel(rows[i]["end_id"], rows[i]["end_cep"], rows[i]["end_rua"], rows[i]["end_bairro"], rows[i]["end_numero"], rows[i]["end_cidade"], rows[i]["end_estado"], rows[i]["end_complemento"], rows[i]["createdAt"], rows[i]["updatedAt"]));
+            lista.push(new EnderecoModel(rows[i]["end_id"], rows[i]["end_cep"], rows[i]["end_rua"], rows[i]["end_bairro"], rows[i]["end_numero"], rows[i]["end_cidade"], rows[i]["end_estado"], rows[i]["end_complemento"], rows[i]["pess_id"], rows[i]["createdAt"], rows[i]["updatedAt"]));
         }
 
         return lista;
@@ -91,6 +95,7 @@ class EnderecoModel {
                 row["end_cidade"],
                 row["end_estado"],
                 row["end_complemento"],
+                row["pess_id"],
                 row["createdAt"],
                 row["updatedAt"]);
 
@@ -98,11 +103,11 @@ class EnderecoModel {
 
     }
 
-    async cadastrarEndereço() {
+    async cadastrarEndereco() {
         if (this.#end_id == 0) {
-            let sql = "INSERT INTO tb_endereco (end_cep, end_rua, end_bairro, end_numero, end_cidade, end_estado, end_complemento, createdAt, updatedAt) VALUES (?,?,?,?,?,?,?,?,?,)";
+            let sql = "INSERT INTO tb_endereco (end_cep, end_rua, end_bairro, end_numero, end_cidade, end_estado, end_complemento, pess_id, createdAt, updatedAt) VALUES (?,?,?,?,?,?,?,?,?,?)";
 
-            let valores = [this.#end_cep, this.#end_rua, this.#end_bairro, this.#end_numero, this.#end_cidade, this.#end_estado, this.#end_complemento, this.#createdAt, this.#updatedAt];
+            let valores = [this.#end_cep, this.#end_rua, this.#end_bairro, this.#end_numero, this.#end_cidade, this.#end_estado, this.#end_complemento, this.#pess_id, this.#createdAt, this.#updatedAt];
 
             let result = await banco.ExecutaComandoNonQuery(sql, valores);
 
@@ -112,9 +117,9 @@ class EnderecoModel {
     }
 
     async editarEndereco() {
-        let sql = "UPDATE tb_endereco SET end_cep = ?, end_rua = ?, end_bairro = ?, end_numero = ?, end_cidade = ?, end_estado = ?, end_complemento = ?, createdAt = ?, updatedAt = ? WHERE end_id = ?";
+        let sql = "UPDATE tb_endereco SET end_cep = ?, end_rua = ?, end_bairro = ?, end_numero = ?, end_cidade = ?, end_estado = ?, end_complemento = ?, pess_id = ?, createdAt = ?, updatedAt = ? WHERE end_id = ?";
 
-        let valores = [this.#end_cep, this.#end_rua, this.#end_bairro, this.#end_numero, this.#end_cidade, this.#end_estado, this.#end_complemento, this.#createdAt, this.#updatedAt, this.#end_id];
+        let valores = [this.#end_cep, this.#end_rua, this.#end_bairro, this.#end_numero, this.#end_cidade, this.#end_estado, this.#end_complemento, this.#pess_id, this.#createdAt, this.#updatedAt, this.#end_id];
 
         let result = await banco.ExecutaComandoNonQuery(sql, valores);
 
