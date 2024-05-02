558492733807d5dbbe904518fe7dd3a9ff6e6190
victorhugoRUB <victor.santos@rubcube.com>
2024-05-02 19:14:04 -0300

    fix: routes

--- routes/projetosRoutes.js
+++ routes/projetosRoutes.js
@@ -8,6 +8,8 @@ let router = express.Router();
 router.get('/listar', ctrl.listagemView);
 router.get('/cadastrar', ctrl.listagemAtivCad, ctrl.cadastroView)
 router.post('/cadastrar', ctrl.cadastrar);
+router.get('/alterar/:id', ctrl.alterarView)
+router.post('/alterar', ctrl.alterar) 
 router.post('/excluir', ctrl.excluir) 
 
 module.exports = router;
